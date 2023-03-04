13d7145ff63c5de4f02b6710d30c1239f67c833a
gogi999 <g.petrovic9@gmail.com>
2023-03-04 19:01:33 +0100

    Added basic setup and Navbar component

---
+++ src/assets/index.js
@@ -0,0 +1,61 @@
+import backend from './backend.png';
+import carrent from './carrent.png';
+import close from './close.svg';
+import meta from './company/meta.png';
+import shopify from './company/shopify.png';
+import starbucks from './company/starbucks.png';
+import tesla from './company/tesla.png';
+import creator from './creator.png';
+import g9_logo from './g9-logo.png';
+import github from './github.png';
+import jobit from './jobit.png';
+import logo from './logo.svg';
+import menu from './menu.svg';
+import mobile from './mobile.png';
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import threejs from './tech/threejs.svg';
+import typescript from './tech/typescript.png';
+import tripguide from './tripguide.png';
+import web from './web.png';
+
+export {
+  backend,
+  carrent,
+  close,
+  creator,
+  css,
+  docker,
+  figma,
+  g9_logo,
+  git,
+  github,
+  html,
+  javascript,
+  jobit,
+  logo,
+  menu,
+  meta,
+  mobile,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  shopify,
+  starbucks,
+  tailwind,
+  tesla,
+  threejs,
+  tripguide,
+  typescript,
+  web,
+};
